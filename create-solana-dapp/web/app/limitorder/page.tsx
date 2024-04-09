40708bb199f33a8ccb1e484d8fc6225519fb59f7
vivekjami <j.vivekvamsi@gmail.com>
2024-04-09 10:45:31 +0530

    extra

--- create-solana-dapp/web/app/limitorder/page.tsx
+++ create-solana-dapp/web/app/limitorder/page.tsx
@@ -1,101 +1,178 @@
 // TypeScript
 "use client"
 // TypeScript
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useWallet } from '@solana/wallet-adapter-react';
 import axios from 'axios';
+import { LimitOrderProvider, Order } from "@jup-ag/limit-order-sdk";
+import { Decimal } from "decimal.js";
+import {
+  AddressLookupTableAccount,
+  Connection,
+  Keypair,
+  PublicKey,
+  TransactionMessage,
+  VersionedTransaction,
+} from "@solana/web3.js";
+import { getQuote, getSwapIx } from "./jupiterApi";
+import { Wallet, BN } from "@coral-xyz/anchor";
+import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+import { config } from "dotenv";
+import { getTakerFee } from "./fee";
+config();
 
-export default function LimitOrder() {
-  const [asset, setAsset] = useState('SOL');
-  const [price, setPrice] = useState(0);
-  const [size, setSize] = useState(0);
+export function MainComponent() {
+  const { publicKey } = useWallet();
 
-  const wallet = useWallet();
-
-  const handleAssetChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setAsset(event.target.value);
-  };
-
-  const handlePriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPrice(Number(event.target.value));
-  };
-
-  const handleSizeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSize(Number(event.target.value));
-  };
-
-  const placeLimitOrder = async () => {
-    if (!wallet.connected) {
-      console.error('Wallet is not connected');
+  useEffect(() => {
+    if (!publicKey) {
+      console.log("Please connect a wallet");
       return;
     }
-  
-    const order = {
-      market: 'MARKET_SYMBOL', // Replace with the actual market symbol
-      side: 'buy', // or 'sell'
-      price: price,
-      size: size,
-      type: 'limit',
-    };
-  
-    try {
-      const publicKey = wallet.publicKey;
-      if (!publicKey) {
-        console.error('Wallet public key is null');
-        return;
-      }
-      
-      const response = await axios.post('https://api.jupiter.com/orders', order, {
-        headers: {
-          'Authorization': `Bearer ${publicKey.toBase58()}`, // Replace with the actual method to get the JWT token
-        },
-      });
-  
-      console.log('Order placed:', response.data);
-    } catch (error) {
-      console.error('Error placing order:', error);
-  
-      // Handle the error based on its type
-      if (axios.isAxiosError(error)) {
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          console.error(error.response.data);
-          console.error(error.response.status);
-          console.error(error.response.headers);
-        } else if (error.request) {
-          // The request was made but no response was received
-          console.error(error.request);
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          console.error('Error', error.message);
+
+    const main = async () => {
+      try {
+        const connection = new Connection(
+          `https://api.helius.xyz/v0/addresses/${publicKey}/transactions?api-key=b956a5d5-4c8f-4caa-9956-3c36d31d17bc`
+        );
+        const limitOrder = new LimitOrderProvider(connection);
+        const fee = await limitOrder.getFee();
+
+        while (true) {
+      // get open orders
+      let pendingOrders = await limitOrder.getOrders();
+      let filterOrders: { publicKey: PublicKey; account: Order }[] = [];
+
+      // group orders according to pair
+      const pendingOrderGroup = pendingOrders.reduce((group, order) => {
+        const {
+          account: { inputMint, outputMint },
+        } = order;
+        const pair = inputMint.toBase58() + outputMint.toBase58();
+
+        group[pair] = group[pair] || [];
+        group[pair].push(order);
+        return group;
+      }, {});
+
+      // filter top order from each pair
+      Object.values(pendingOrderGroup).forEach(
+        (orders: { publicKey: PublicKey; account: Order }[]) => {
+          const sortOrders = orders.sort((a, b) => {
+            a.account.takingAmount.cmp;
+            const aPrice = new Decimal(a.account.takingAmount.toString()).div(
+              a.account.makingAmount.toString()
+            );
+            const bPrice = new Decimal(b.account.takingAmount.toString()).div(
+              b.account.makingAmount.toString()
+            );
+            return aPrice.cmp(bPrice);
+          });
+
+          filterOrders = [...sortOrders.slice(0, 1), ...filterOrders];
         }
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.error('Error', error as Error);
+      );
+
+      for (let order of pendingOrders) {
+        const {
+          account: {
+            makingAmount,
+            takingAmount,
+            inputMint,
+            outputMint,
+            makerOutputAccount,
+          },
+          publicKey,
+        } = order;
+
+        console.log("Try to execute order", publicKey.toBase58());
+
+        // return if maker output token account has closed
+        const makerOutputAccountInfo = await connection.getAccountInfo(
+          makerOutputAccount
+        );
+        if (!makerOutputAccountInfo) return;
+
+        // get route from Jupiter quote api
+        const route = await getQuote(
+          inputMint,
+          outputMint,
+          makingAmount.toString()
+        );
+        if (!route) return;
+        const quoteOutAmount = new BN(route.outAmount);
+
+        // calculate taking amount with taker fee
+        const takerFee = getTakerFee(inputMint, outputMint, fee);
+        const takingAmountWithTakerFee = takingAmount
+          .muln(10000 + takerFee)
+          .divn(10000);
+
+        // execute if jupiter quote out amount is greater than taking amount with taker fee
+        if (quoteOutAmount.gte(takingAmountWithTakerFee)) {
+          // get swap transaction from jupiter api and deserialize
+          let { swapTransaction: rawTx } = await getSwapIx(
+            publicKey, // Access the publicKey property of the wallet object
+            route
+          );
+
+          const swapTransactionBuf = Buffer.from(rawTx, "base64");
+          const swapTransaction =
+            VersionedTransaction.deserialize(swapTransactionBuf);
+
+          const swapALT = await Promise.all(
+            swapTransaction.message.addressTableLookups.map(async (lookup) => {
+              return new AddressLookupTableAccount({
+                key: lookup.accountKey,
+                state: AddressLookupTableAccount.deserialize(
+                  await connection
+                    .getAccountInfo(lookup.accountKey)
+                    .then((res) => res!.data)
+                ),
+              });
+            })
+          );
+          const txMessage = TransactionMessage.decompile(
+            swapTransaction.message,
+            {
+              addressLookupTableAccounts: swapALT,
+            }
+          );
+
+          // get execute limit order transaction
+          const limitOrderTx = await limitOrder.fillOrder({
+            owner: publicKey,
+            orderAccount: order,
+            amount: makingAmount,
+            expectedOutAmount: takingAmount,
+          });
+
+          // combine limit order and swap transaction
+          txMessage.instructions.push(...limitOrderTx.instructions);
+          txMessage.recentBlockhash = (
+            await connection.getLatestBlockhash()
+          ).blockhash;
+          swapTransaction.message = txMessage.compileToV0Message([...swapALT]);
+
+          swapTransaction.sign([Wallet.payer]);
+
+          const txid = await connection.sendRawTransaction(
+            swapTransaction.serialize()
+          );
+
+          console.log({ txid });
+        }
+
+        await new Promise((resolve) => setTimeout(resolve, 1000));
       }
     }
-  };
+      } catch (err) {
+        console.error({ err });
+      }
+    };
 
-  return (
-    <div style={{ padding: '20px', backgroundColor: '#2F76DB' }}>
-      <div>
-        <label>Asset</label>
-        <select value={asset} onChange={handleAssetChange}>
-          <option value="SOL">SOL</option>
-          <option value="USDC">USDC</option>
-          {/* Add more options as needed */}
-        </select>
-      </div>
-      <div>
-        <label>Price</label>
-        <input type="number" value={price} onChange={handlePriceChange} />
-      </div>
-      <div>
-        <label>Size</label>
-        <input type="number" value={size} onChange={handleSizeChange} />
-      </div>
-      <button style={{ padding: '10px', backgroundColor: '#1F2D5A', color: 'white', border: 'none', borderRadius: '5px' }} onClick={placeLimitOrder}>Place Limit Order</button>
-    </div>
-  );
+    main();
+  }, [publicKey]);
+
+  return null;
 }
